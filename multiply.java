0de878f08fe459cd8dfbd4b25e5a2f8474b1f20c
Pranoti Patil <patilpranoti1812@gmail.com>
2023-03-16 05:06:31 -0700

    added  multiply.java

---
+++ multiply.java
@@ -0,0 +1,9 @@
+public class multiply {
+    public static void main(String[] args) {
+     
+        int num1 = 5, num2 = 15, mul;
+        mul = num1 * num2;
+     
+        System.out.println("The mul of two numbers no1*no2=: "+mul);
+     }
+}
